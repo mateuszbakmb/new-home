9c3b3a4a5f18e6ece06716d4588ef9d5b8931d73
mateuszbak.mb <mateuszbak.mb@gmail.com>
2019-02-03 18:01:03 +0100

    navbar transition effect while scrolling

--- script/script.js
+++ script/script.js
@@ -1,57 +1,79 @@
-//scrolling from navbar
+document.addEventListener("DOMContentLoaded", ()=>{
 
-let navbar = document.querySelector(".navbar")
-let href = navbar.querySelectorAll("a[href]").forEach(elem=> {
-  elem.addEventListener('click', moveTo)
+  const nav = document.querySelector('nav');
 
-  function moveTo(e){
-    e.preventDefault();
-    document.querySelector(elem.getAttribute('href')).scrollIntoView({behavior: 'smooth', block: 'start'});
+  window.addEventListener('scroll', function(event) {
+      event.preventDefault();
+
+      if (window.scrollY >= nav.offsetHeight) {
+          nav.classList.add('navMoved')
+          // nav.classList.remove('staticNav')
+      } else {
+          nav.classList.remove('navMoved')
+          // nav.classList.add('staticNav')
+      }
+  });
+
+
+
+  //scrolling from navbar
+
+  let navbar = document.querySelector(".navbar")
+  let href = navbar.querySelectorAll("a[href]").forEach(elem=> {
+    elem.addEventListener('click', moveTo)
+
+    function moveTo(e){
+      e.preventDefault();
+      document.querySelector(elem.getAttribute('href')).scrollIntoView({behavior: 'smooth', block: 'start'});
+    }
+  })
+
+  //animated number
+
+  let projectsAmount = document.querySelector('#projectsCounter')
+  const scrolling = function(e) {
+      let elementPos = projectsAmount.getBoundingClientRect().y - window.innerHeight + projectsAmount.getBoundingClientRect().height;
+      if (elementPos >= -600 && elementPos <= 20) {
+          countUpProjects();
+          countUpSatisfied();
+          window.removeEventListener('scroll', scrolling)
+      }
   }
-})
 
-//animated number
+  window.addEventListener("scroll", scrolling);
 
-let projectsAmount = document.querySelector('#projectsCounter')
-const scrolling = function(e) {
-    let elementPos = projectsAmount.getBoundingClientRect().y - window.innerHeight + projectsAmount.getBoundingClientRect().height;
-    if (elementPos >= -600 && elementPos <= 20) {
-        countUpProjects();
-        countUpSatisfied();
-        window.removeEventListener('scroll', scrolling)
-    }
-}
-
-window.addEventListener("scroll", scrolling);
-
-function countUpProjects () {
-let customers = document.querySelector('#projectsCounter')
-let current = 0;
-let end = 200;
-
-  let animating = setInterval(function(){
-    current++;
-    customers.innerHTML = current;
-    if(current === end){
-      clearInterval(animating);
-    }
-  }, 10)
-}
-
-function countUpSatisfied(){
-  let satisfied = document.querySelector('#satisfiedCounter')
-    let current = 0;
-    let end = 1000;
+  function countUpProjects () {
+  let customers = document.querySelector('#projectsCounter')
+  let current = 0;
+  let end = 200;
 
     let animating = setInterval(function(){
       current++;
-      satisfied.innerHTML = current;
+      customers.innerHTML = current;
       if(current === end){
         clearInterval(animating);
       }
-    }, 1)
-}
+    }, 10)
+  }
 
-window.onbeforeunload = ()=> {
-  window.scrollTo(0, 0);
-}
+  function countUpSatisfied(){
+    let satisfied = document.querySelector('#satisfiedCounter')
+      let current = 0;
+      let end = 1000;
+
+      let animating = setInterval(function(){
+        current++;
+        satisfied.innerHTML = current;
+        if(current === end){
+          clearInterval(animating);
+        }
+      }, 1)
+  }
+
+  window.onbeforeunload = ()=> {
+    window.scrollTo(0, 0);
+  }
+
+
+
+})
